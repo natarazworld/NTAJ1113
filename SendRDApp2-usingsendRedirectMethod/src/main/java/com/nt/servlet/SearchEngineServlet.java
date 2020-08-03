2e9e923680526c24acb81196ebe4c092ef3368fc
natarazworld <natarazworld@gmail.com>
2020-08-03 12:36:46 +0530

    SEndRedirection

---
+++ SendRDApp2-usingsendRedirectMethod/src/main/java/com/nt/servlet/SearchEngineServlet.java
@@ -0,0 +1,44 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/searchurl")
+public class SearchEngineServlet extends HttpServlet {
+
+       
+
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	   String ss=null;
+	   String engine=null;
+	   String url=null;
+		 //read form data
+	   ss=req.getParameter("ss");
+	   engine=req.getParameter("engine");
+	    //prepare url
+	   if(engine.equalsIgnoreCase("google"))
+		    url="https://www.google.com/search?q="+ss;
+	   else if(engine.equalsIgnoreCase("bing"))
+		   url="https://www.bing.com/search?q="+ss;
+	   else if(engine.equalsIgnoreCase("yahoo"))
+		     url="https://in.search.yahoo.com/search?p="+ss;
+	   else
+		   url="https://duckduckgo.com/?q="+ss;
+		 //perform sendRedirection
+	   res.sendRedirect(url);
+		
+	}
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, res);
+	}
+
+}
